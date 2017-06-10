dbe4be153f99aae966e4972f05b2c1562f8a0042
Juan Pablo Enrici <jpenrici@gmail.com>
2017-06-10 10:42:33 -0300

    Create growthPopulation.cpp

--- growthPopulation.cpp
+++ growthPopulation.cpp
@@ -25,7 +25,7 @@ It will need 3 entire years.
 More generally given parameters:
 
 p0, percent, aug (inhabitants coming or leaving each year),
-					p (population to surpass)
+p (population to surpass)
 
 the function nb_year should return n number of entire years needed to
 get a population greater or equal to p.
